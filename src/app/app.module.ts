16243a30181a5f425fd732eb27d9b28bd0b60b38
Geert Huls <geert.huls82@gmail.com>
2017-08-25 08:38:03 +0200

    Created settings component
    - ng g c settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './settings/settings.component';
+import { PanelModule } from 'primeng/primeng';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]

2d133e6fbb6f16dcebd3cff80120c2422ad4aec0
Geert Huls <geert.huls82@gmail.com>
2017-08-29 22:20:56 +0200

    Replaced hard-coded data with projections

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+const DEFAULT_COLORS = ['#3366CC', '#DC3912', '#FF9900', '#109618', '#990099',
+  '#3B3EAC', '#0099C6', '#DD4477', '#66AA00', '#B82E2E',
+  '#316395', '#994499', '#22AA99', '#AAAA11', '#6633CC',
+  '#E67300', '#8B0707', '#329262', '#5574A6', '#3B3EAC'];
 
 @Component({
   selector: 'app-dashboard',
@@ -7,19 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent {
 
-  hoursByProject = [
+  private hoursByProject = [
     { id: 1, name: 'Payroll App', hoursSpent: 8 },
     { id: 2, name: 'Agile Times App', hoursSpent: 16 },
     { id: 3, name: 'Point of Sale App', hoursSpent: 24 },
   ];
 
-  hoursByProjectChartData = {
-    labels: ['Payroll App', 'Agile Times App', 'Point of Sale App'],
+  private pieLabels = this.hoursByProject.map((proj) => proj.name);
+  private pieData = this.hoursByProject.map((proj) => proj.hoursSpent);
+  private pieColors = this.configureDefaultColours(this.pieData);
+
+  private hoursByProjectChartData = {
+    labels: this.pieLabels,
     datasets: [
       {
-        data: [8, 16, 24],
-        backgroundColor: ['#3366CC', '#DC3912', '#FF9900']
+        data: this.pieData,
+        backgroundColor: this.pieColors
       }
     ]
   };
+
+  private configureDefaultColours(data: number[]): string[] {
+    let customColours = []
+    if (data.length) {
+
+      customColours = data.map((element, idx) => {
+        return DEFAULT_COLORS[idx % DEFAULT_COLORS.length];
+      });
+    }
+
+    return customColours;
+  }
 }

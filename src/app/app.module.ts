82da2073f4917920007336df2901484c7083125e
Geert Huls <geert.huls82@gmail.com>
2017-08-31 21:44:42 +0200

    Added markup for projects form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SettingsComponent } from './settings/settings.component';
-import { MenuModule, PanelModule, ChartModule } from 'primeng/primeng';
+import { MenuModule, PanelModule, ChartModule, InputTextModule } from 'primeng/primeng';
 import { routing } from './routing';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -29,7 +29,8 @@ import { ProjectsComponent } from './projects/projects.component';
     MenuModule,
     PanelModule,
     ChartModule,
-    routing
+    routing,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]

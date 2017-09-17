6562f35c6a95cf65e4e2ffc9b7d20bd91dd73334
Geert Huls <geert.huls82@gmail.com>
2017-09-17 22:24:17 +0200

    Test display profile picture with manual change detecting

--- src/app/profile/profile.component.spec.ts
+++ src/app/profile/profile.component.spec.ts
@@ -4,6 +4,7 @@ import { ProfileComponent } from './profile.component';
 import { DragDropModule, GalleriaModule, GrowlModule, PanelModule } from 'primeng/primeng';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import createSpy = jasmine.createSpy;
+import { By } from '@angular/platform-browser';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
@@ -32,5 +33,21 @@ describe('ProfileComponent', () => {
 
     this.component.onDragStart(mockGalleria);
     expect(mockGalleria.stopSlideshow).toHaveBeenCalled();
+  });
+
+  it('should update the image on drop', () => {
+    const mockGalleria = {
+      activeIndex: 2,
+      stopSlideshow: createSpy('stopSlideshow')
+    };
+
+    this.component.onDragStart(mockGalleria);
+    this.component.onPicDrop();
+
+    fixture.detectChanges();
+
+    expect(component.profileImage).toEqual('http://i.pravatar.cc/300?u=Mary');
+    const imgElement = fixture.debugElement.query(By.css('#profilePic')).nativeElement;
+    expect(imgElement).toBeTruthy();
   });
 });

6b2a52a4682a581ff3113be009173ce286bc3d60
Geert Huls <geert.huls82@gmail.com>
2017-09-17 22:18:18 +0200

    Profile on drag start test + removed redundant tests

--- src/app/profile/profile.component.spec.ts
+++ src/app/profile/profile.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileComponent } from './profile.component';
+import { DragDropModule, GalleriaModule, GrowlModule, PanelModule } from 'primeng/primeng';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import createSpy = jasmine.createSpy;
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
@@ -8,7 +11,9 @@ describe('ProfileComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfileComponent ]
+      declarations: [ ProfileComponent ],
+      imports: [ GrowlModule, GalleriaModule, DragDropModule, PanelModule,
+        NoopAnimationsModule]
     })
     .compileComponents();
   }));
@@ -19,7 +24,13 @@ describe('ProfileComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+  it('should stop the slideshow on starting drag', () => {
+    const mockGalleria = {
+      activeIndex: 2,
+      stopSlideshow: createSpy('stopSlideshow')
+    };
+
+    this.component.onDragStart(mockGalleria);
+    expect(mockGalleria.stopSlideshow).toHaveBeenCalled();
   });
 });
